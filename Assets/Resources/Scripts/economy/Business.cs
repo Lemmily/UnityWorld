1f25e7c5d2a7f9e066131ab9378e9aba28341545
James <jy1v07@gmail.com>
2016-01-10 17:35:14 +0000

    start of business stuff

---
+++ Assets/Resources/Scripts/economy/Business.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Business{
+    private string businessName = "";
+    private Economy.Resource productionResource;
+    private int points;
+    private int updateSpeed = 5;
+    private Dictionary<Economy.Resource,double> resources = null;   //Dictionary of Resources held by business, <name,quantity>
+
+    public Business(string name, Economy.Resource res)
+    {
+        create(name, res);
+    }
+
+    // Use this for initialization
+    //void Start()
+    //{
+    //    points = 0;
+    //    create();
+    //}
+
+    // Creation of a Business entity - default params
+    void create(string name, Economy.Resource res)
+    {
+        resources = new Dictionary<Economy.Resource, double>();
+        businessName = name;
+        productionResource = res;
+        resources.Add(res,0.0);
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+
+
+    public void gainActionPoints(int points)
+    {
+        Debug.Log(businessName+ " gained points");
+        this.points += points;
+    }
+}
+
--- Assets/Resources/Scripts/economy/Economy.cs
+++ Assets/Resources/Scripts/economy/Economy.cs
@@ -7,12 +7,22 @@ public class Economy : MonoBehaviour {
     private Dictionary<Resource, Auction> auctionByType;
     private int updateSpeed = 5;
 
+    private List<Business> businessList = null;
+
     // Use this for initialization
     void Start () {
         points = 0;
         auctionByType = new Dictionary<Resource, Auction>();
         auctions = new List<Auction>();
-	}
+
+
+        //Create Businesses
+        businessList = new List<Business>();
+        businessList.Add(new Business("Emily's Ripe Peaches", Resource.POTTERY));
+        businessList.Add(new Business("Emily's Sour Plums", Resource.IRON));
+        businessList.Add(new Business("Emily's Spicy Nachos", Resource.UNKNOWN));
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,11 +30,15 @@ public class Economy : MonoBehaviour {
             int count = auctions.Count;
             for (var i = 0; i < count; i++) auctions[i].Update(points);
         }
-}
+    }
 
     public void gainActionPoints(int points) {
         Debug.Log("Economy gained points");
         this.points += points;
+        foreach(Business b in businessList)
+        {
+            b.gainActionPoints(points);
+        }
     }
 
 
