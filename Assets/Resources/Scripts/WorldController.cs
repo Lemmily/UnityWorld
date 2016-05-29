a53dc584719ac85b6bd6f6f41ee58a779db1f8b9
Emily <lemmily0@gmail.com>
2016-05-29 16:34:05 +0100

    added font and some psds sorted out scene files

---
+++ Assets/Resources/Scripts/WorldController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class WorldController : MonoBehaviour {
+    public static WorldController Instance
+    {
+        get;
+        protected set;
+    }
+
+    internal static WorldController instance;
+    public MapInfo mapInfo;
+
+    // Use this for initialization
+    void OnEnable () {
+        Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    internal MapInfo GetCurrentMapInfo() {
+        return mapInfo;
+    }
+
+    public void OnMapChange() {
+
+    }
+}
