c4a48f674a47b401974495c7f5aec1c57a058b97
Emily Johnson <emily.johnson@tcsjohnhuxley.com>
2016-09-02 14:29:52 +0100

    completely untested, added in potential for doing the mesh stuff - want a setup to test betweeen mesh/pooling methods

---
+++ Assets/Resources/Scripts/Controller/WorldTimeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public class WorldTimeController : MonoBehaviour
+{
+    int turnsFromBeginning = 0;
+    float timePassed= 0;
+    private bool waitForPlayer;
+    private float updateSpeed = 1f;
+
+    void Start() {
+        waitForPlayer = false;
+    }
+
+    void Update() {
+        if ( ! waitForPlayer) {
+            timePassed += Time.deltaTime;
+            if (timePassed > updateSpeed) {
+                timePassed = 0;
+                advanceTime(1);
+            }
+
+        }
+    }
+
+    //date tracking need to be in here too.
+
+
+
+    // "time" here is an abstract measure. maybe "points" is a better word.
+    public void advanceTime(int points) {
+        BroadcastMessage("gainActionPoints", points);
+        turnsFromBeginning += points;
+    }
+
+    internal void TogglePause() {
+        waitForPlayer = !waitForPlayer;
+        Debug.Log("Toggled Pause to " + waitForPlayer.ToString() + "!");
+    }
+}
