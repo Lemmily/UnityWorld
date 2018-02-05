78db31cd3d13d577e4852a6a16160767e611bb26
Emily Johnson <lemmily@gmail.com>
2018-02-05 20:25:50 +0000

    uhoh left it ages

---
+++ Assets/Resources/Scripts/PathFinder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder {
+
+    private World world;
+
+    public Pathfinder(World world)
+    {
+        this.world = world; //we need access to all the info here.
+        //nodify?
+        //each node needs to know if a tile has been changed
+    }
+    
+	
+}
